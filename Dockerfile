cc9cb4ece38463c3c75875148a7dd95c9f4df39b
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2025-12-20 14:06:01 +0100

    Use Azure Insight with OpenTelemetry

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ WORKDIR /app
 ARG OTEL_JAVA_AGENT_VERSION=1.32.0
 ADD https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v${OTEL_JAVA_AGENT_VERSION}/opentelemetry-javaagent.jar /otel/opentelemetry-javaagent.jar
 COPY --from=build /app/target/*.jar /app/app.jar
+COPY agent/applicationinsights-agent-3.7.4.jar applicationinsights-agent-3.7.4.jar
 ENV JAVA_TOOL_OPTIONS="-javaagent:/otel/opentelemetry-javaagent.jar"
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","/app/app.jar"]
+ENTRYPOINT ["java","-javaagent:applicationinsights-agent-3.7.4.jar", "-jar","/app/app.jar"]
--- src/main/java/org/lime/expenseai/config/DataInitializer.java
+++ src/main/java/org/lime/expenseai/config/DataInitializer.java
@@ -15,9 +15,9 @@ public class DataInitializer {
     CommandLineRunner seedExpenses(ExpenseService expenseService) {
         return args -> {
             if (expenseService.getAllExpenses().isEmpty()) {
-                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2024, 6, 2), "FOOD", 30, "Dinner"));
-                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2024, 6, 8), "SHOPPING", 300, "Buy clothes"));
-                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2024, 6, 15), "GROCERIES", 88.99, "Supermarket"));
+                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2025, 6, 2), "FOOD", 30, "Dinner"));
+                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2025, 6, 8), "SHOPPING", 300, "Buy clothes"));
+                expenseService.addExpense(new ExpenseDto(null, LocalDate.of(2025, 6, 15), "GROCERIES", 88.99, "Supermarket"));
             }
         };
     }
